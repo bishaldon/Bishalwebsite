b6f25fea5ea82a8c3b283bbb2c9e82e5eda30eaa
bishaldon <158747962+bishaldon@users.noreply.github.com>
2024-02-03 19:14:29 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+autoplay: {
+    delay: 2000
+},
+breakpoints:{
+    1200:{
+        slidesPerView: 3,
+    },
+    800:{
+        slidesPerView: 2,
+    },
+    500:{
+        slidesPerView: 1,
+    }
+},
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
